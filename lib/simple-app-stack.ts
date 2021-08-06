1f8c21b18d13894ee63fe805a8471c941d88aabb
Rodolfo Goncalves <rodolfo.goncalves@blip.pt>
2021-08-06 17:56:57 +0100

    api gateway

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -5,11 +5,21 @@ import * as lambdaNodejs from "@aws-cdk/aws-lambda-nodejs";
 import * as lambda from "@aws-cdk/aws-lambda";
 import { BucketDeployment, Source } from "@aws-cdk/aws-s3-deployment";
 import { Policy, PolicyStatement } from "@aws-cdk/aws-iam";
+import {
+  HttpApi,
+  HttpIntegration,
+  HttpMethod,
+  CorsHttpMethod,
+} from "@aws-cdk/aws-apigatewayv2";
+import { LambdaProxyIntegration } from "@aws-cdk/aws-apigatewayv2-integrations";
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    /** *******************************
+     *      S3 bucket with photos     *
+     **********************************/
     const bucket = new s3.Bucket(this, "MySimpleAppS3BucketName", {
       encryption: s3.BucketEncryption.S3_MANAGED,
     });
@@ -24,6 +34,9 @@ export class SimpleAppStack extends cdk.Stack {
       exportName: "MySimpleAppS3BucketName",
     });
 
+    /** *************************
+     *      Lambda Function     *
+     ****************************/
     const getPhotosLambdaFn = new lambdaNodejs.NodejsFunction(
       this,
       "MySimpleAppgetPhotosLambdaFn",
@@ -37,6 +50,9 @@ export class SimpleAppStack extends cdk.Stack {
       }
     );
 
+    /** ******************************
+     *      IAM role permissions     *
+     *********************************/
     const bucketContainerPermissions = new PolicyStatement();
     bucketContainerPermissions.addResources(bucket.bucketArn);
     bucketContainerPermissions.addActions("s3:ListBucket");
@@ -47,5 +63,48 @@ export class SimpleAppStack extends cdk.Stack {
 
     getPhotosLambdaFn.addToRolePolicy(bucketContainerPermissions);
     getPhotosLambdaFn.addToRolePolicy(bucketPermissions);
+
+    /** *************
+     *      API     *
+     ****************/
+    const httpApi = new HttpApi(this, "MySimpleAppHttpApi", {
+      corsPreflight: {
+        allowOrigins: ["*"],
+        allowMethods: [CorsHttpMethod.GET],
+      },
+      apiName: "photo-api",
+      createDefaultStage: true,
+    });
+    const getPhotosLambdaIntegration = new LambdaProxyIntegration({
+      handler: getPhotosLambdaFn,
+    });
+
+    httpApi.addRoutes({
+      path: "/photos",
+      methods: [HttpMethod.GET],
+      integration: getPhotosLambdaIntegration,
+    });
+
+    new cdk.CfnOutput(this, "MySimpleAppAPI", {
+      value: httpApi.url!,
+      exportName: "MySimpleAppAPIEndpoint",
+    });
+
+    /** *******************************
+     *      S3 website bucket     *
+     **********************************/
+    // const websiteBucket = new s3.Bucket(
+    //   this,
+    //   "MySimpleAppWebsiteS3BucketName",
+    //   {
+    //     websiteIndexDocument: "index.html",
+    //     publicReadAccess: true,
+    //   }
+    // );
+
+    // new BucketDeployment(this, "MySimpleAppWebsite", {
+    //   sources: [Source.asset(path.join(__dirname, "..", "frontend", "build"))],
+    //   destinationBucket: bucket,
+    // });
   }
 }
